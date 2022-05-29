d1190cfd17283cf7a20538981ef4a29c8a5d7e65
BhrunoB8 <bhrunoroan@yahoo.com.br>
2022-05-29 13:21:44 -0300

    Somente um print em cada função do server

--- socketRay/client.py
+++ socketRay/client.py
@@ -1,5 +1,4 @@
 import socket
-from time import sleep
 HOST = "127.0.0.1"
 PORT = 50000
 
@@ -11,29 +10,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.sendall((bytes("Monitorar Memória", "utf-8")))
         data = s.recv(1024)
         print("Received:\n", data.decode())
-        while True:
-            s.sendall(bytes("Monitorar Memória", "utf-8"))
-            data = s.recv(1024)
-            print("Received:\n", data.decode())
-            sleep(5)
-    if input() == "Monitorar CPU":
+    elif input() == "Monitorar CPU":
         s.sendall((bytes("Monitorar CPU", "utf-8")))
         data = s.recv(1024)
         print("Received:\n", data.decode())
-        while True:
-            s.sendall(bytes("Monitorar CPU", "utf-8"))
-            data = s.recv(1024)
-            print("Received:\n", data.decode())
-            sleep(5)
-
-        # sent = input().encode()
-        # s.sendall(sent)
-        # data = s.recv(1024)
-        # print("Received:\n", data.decode())
-
-    # while True:
-    #     sent = input("> ").encode()
-    #     s.sendall(sent)
-    #     data = s.recv(1024)
-    #     print("Received: ", data.decode())
         
--- socketRay/server.py
+++ socketRay/server.py
@@ -1,15 +1,5 @@
 import socket
-from time import sleep
 import psutil
-
-# def get_cpu():
-#     cpufreq = psutil.cpu_freq()
-#     print("CPU Usage Per Core: ")
-#     for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
-#         print(f"Core {i}: {percentage}%")
-#     print(f"Total CPU Usage: {psutil.cpu_percent()}%")
-
-   
 
 def get_size(bytes, suffix="B"):
     factor = 1024
@@ -20,7 +10,7 @@ def get_size(bytes, suffix="B"):
 
 def get_mem_info():
     swap = psutil.swap_memory()
-    return (f"Total:  {get_size(swap.total)} \nFree:  {get_size(swap.free)} \nUsed:  {get_size(swap.used)}\nPercentage:  {swap.percent}%")
+    return (f"\nTotal:  {get_size(swap.total)} \nFree:  {get_size(swap.free)} \nUsed:  {get_size(swap.used)}\nPercentage:  {swap.percent}%")
 
 HOST = "127.0.0.1"
 PORT = 50000
@@ -32,24 +22,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         conn, addr = s.accept()
         with conn:
             print("Connected to", addr)
-            conn.sendall(bytes("Bem-vindo, para monitorar:\n CPU digite: Monitorar CPU\nMemória digite: Monitorar Memória", "utf-8"))
+            conn.sendall(bytes("Bem-vindo, para monitorar:\nCPU digite: Monitorar CPU\nMemória digite: Monitorar Memória", "utf-8"))
             while True:
                 data = conn.recv(1024)
                 if not data: break
                 if(data.decode() == "Monitorar Memória"):
                     print("Received:\n", data.decode())
-                    while True:
-                        print("Received:\n", data.decode())
-                        conn.sendall(bytes(get_mem_info(), "utf-8"))
-                        sleep(5)
-                #         if data.decode() == "Pare": break
+                    conn.sendall(bytes(get_mem_info(), "utf-8"))
                 elif(data.decode() == "Monitorar CPU"):
                     print("Received:\n", data.decode())
-                    while True:
-                        print("Received:\n", data.decode())
-                        conn.sendall(bytes(f"Total CPU Usage: {psutil.cpu_percent()}%", "utf-8"))
-                        sleep(5)   
-                else:
-                    print("Received:\n", data.decode())
-                    sent = input("> ".encode)
-                    conn.sendall(bytes(sent, "utf-8"))
+                    conn.sendall(bytes(f"Total CPU Usage: {psutil.cpu_percent()}%", "utf-8"))   
